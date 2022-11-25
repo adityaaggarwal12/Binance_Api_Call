d9d4bd47f9dd60dc34f36fc1007c1d00078d2254
Aditya Aggarwal <adityaaggarwal12339@gmail.com>
2022-11-26 02:12:48 +0530

    added dynamic params in transaction

--- TransactionWithApi/transactionApi.py
+++ TransactionWithApi/transactionApi.py
@@ -8,27 +8,34 @@ secret_key = bytes(config('SECRET_KEY'),'utf-8')
 api_key = bytes(config('API_KEY'),'utf-8')
 
 #
-# get_ts() \
+# get_ts() 
 # returns the server time in milliseconds
 #
 def get_ts():
     res = requests.get("https://api.binance.com/api/v3/time")
     ts=str(res.json()["serverTime"])
     return ts
+
+
 #
 # get_account_balance
-# @Param: [String] params , [byte encoded] secret_key
+# @Param: [object] params , [byte encoded] secret_key
 # This function will produce hmac sha256 signature
 # @Return: {String} signature - hmac sha256 signature
 #
 def get_HMAC(params,secretKey):
-    paramsB = bytes(params,'utf-8')
+    newParams =""
+
+    for i in params.keys():
+        newParams = newParams + i + "=" + params[i] + "&"
+    newParams = newParams[:-1]
+
+    paramsB = bytes(newParams,'utf-8')
     signature = hmac.new(secretKey, paramsB, hashlib.sha256).hexdigest()
     return format(signature)
 
-ts=get_ts()
 
-total_params = "symbol=BTCUSDT&side=BUY&type=MARKET&quantity=0.1&recvWindow=5000&timestamp="+ts
+
 
 #
 # get_account_balance
@@ -36,12 +43,12 @@ total_params = "symbol=BTCUSDT&side=BUY&type=MARKET&quantity=0.1&recvWindow=5000
 # This function will execute the transaction on Binance 
 # @Return: {Object} response_json - JSON object of transaction details
 #
-def exec_transaction(headers,signature):
+def exec_transaction(params,headers):
     try:
 #        url_base = 'https://api.binance.com/api/v3/order?
         url_base = 'https://testnet.binance.vision/api/v3/order?'
-        req_url = url_base+total_params+"&signature="+signature
-        response = requests.post(req_url,headers=headers)
+        # req_url = url_base+total_params+"&signature="+signature
+        response = requests.post(url_base,headers=headers,params=params)
         response_json = response.json()
         print(response_json)
 
@@ -51,10 +58,32 @@ def exec_transaction(headers,signature):
         print('There was an error in the API call')
         return None
 
+symbol = input("enter the symbol ex BTCUSDT:")
+side = input("enter the side - BUY/SELL:")
+type = input("enter the type ex MARKET/LIMIT:")
+quantity = input("enter the quantity:")
 
-signature=get_HMAC(total_params,secret_key)
+params = {
+    "symbol": symbol,
+    "side": side,
+    "type": type,
+    "quantity": quantity,
+    "recvWindow": "5000",
+}
+if(type=="LIMIT"):
+    price = input("enter the price:")
+    params["price"] = price
+
+ts=get_ts()
+params["timestamp"] = ts
+
+
+signature=get_HMAC(params,secret_key)
 headers = {
     'X-MBX-APIKEY' : api_key
 }
 
-exec_transaction(headers,signature)
+params["signature"] = signature
+
+
+exec_transaction(params,headers)
